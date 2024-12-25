9e3539dabbb8a7b286425ad6b2bac2c74a67a1b8
Ahmet Kaplan <kaplan38@gmail.com>
2024-12-25 17:19:49 +0300

    version update

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_invigilator';
-$plugin->release = '1.1.3';
-$plugin->version = 2024122501;
+$plugin->release = '1.1.4';
+$plugin->version = 2024122502;
 $plugin->requires = 2019052000;
 $plugin->maturity = MATURITY_BETA;
